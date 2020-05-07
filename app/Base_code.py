b9bb370264f83bf663159dbe1964d637fa186986
Nithivarn-Reddy <nithivarn.reddy.shanigaram-1@ou.edu>
2020-05-06 23:57:44 -0500

    project commits from OU digi learn to our private repo

---
+++ app/Base_code.py
@@ -0,0 +1,94 @@
+from collections import deque
+#rows=int(input('enter no of rows'));
+#cols=int(input(('enter no of columns')));
+
+rows,cols=0,0
+start,end=0,0
+points=""
+infectedpatients_location=[]
+
+import random
+import flask
+from flask import request
+from json import dumps
+
+
+value =''
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/post/values",methods=["POST"])
+def set_values():
+    if not request.json:
+        global rows,cols,start,end,points,infectedpatients_location
+        rows,cols=10,10
+        start=1
+        end=3
+        points="(0,0)"
+        infectedpatients_location=[(0,0)]
+    dic ={}
+    rows= int(request.json.get('rows'))
+    dic["rows"] = rows
+    cols = int(request.json.get('cols'))
+    dic["cols"] = cols
+    start = int(request.json.get('start'))
+    dic["start"] = start
+    end = int(request.json.get('end'))
+    dic["end"] = end
+    points = request.json.get('points')
+    import re
+    z = re.findall("[0-9]+", points)
+    for i in range(0, len(z), 2):
+        infectedpatients_location.append((int(z[i]), int(z[i + 1])))
+    dic["points"] = infectedpatients_location
+    return dumps(dic)
+
+@app.route("/get_final_infected",methods=["GET"])
+def calculate_display():
+    matrix = [[0 for j in range(cols)] for i in range(rows)];
+    antibody=[]
+    for i in range(rows):
+        x,y=random.randint(0,rows),random.randint(0,cols);
+        antibody.append([x,y]);
+
+
+    for x,y in antibody:
+        matrix[x][y]='A';
+
+    #infectedpatients_location=[[1,1],[5,5],[7,8]]
+
+
+    queue=deque();
+    for x,y in infectedpatients_location:
+
+        queue.append((x,y,0));
+    infectedpatients=len(queue);
+    starttime=2;
+    startpatientcount=0
+    endpatientcount=0;
+    endtime=4
+    while(queue):
+        x,y,time=queue.popleft();
+        if(time==starttime):
+            startpatientcount=infectedpatients;
+        if(time==endtime):
+            endpatientcount=infectedpatients;
+            break;
+        for dx,dy in [[1,0],[-1,0],[0,1],[0,-1],[-1,-1],[-1,1],[1,1],[1,-1]]:
+            if(0<=x+dx<rows and 0<=y+dy<cols ):
+                if(matrix[x+dx][y+dy]==0):
+                    matrix[x+dx][y+dy]=1;
+                    infectedpatients+=1;
+                    queue.append((x+dx,y+dy,time+1));
+    dic = {}
+    dic["matrix"] = matrix
+    dic["startpatientcount"]=startpatientcount
+    dic["endpatientcount"]=endpatientcount
+    dic["infected_patients"] = endpatientcount - startpatientcount
+    #return str(matrix);
+    #return value
+    #print(startpatientcount,endpatientcount)
+    #print(endpatientcount- startpatientcount);
+    return dumps(dic)
+if __name__ == "__main__":
+    app.run(host ='0.0.0.0',port=5010,debug=True)
