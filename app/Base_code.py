0f4a56c5d2d1c701c552a91128c2fe2e3da54959
shivasrk12345 <shivasrk1234@ou.edu>
2020-05-08 00:04:17 -0500

    Host changed

--- app/Base_code.py
+++ app/Base_code.py
@@ -104,4 +104,4 @@ def calculate_display():
     #print(endpatientcount- startpatientcount);
     return dumps(dic)
 if __name__ == "__main__":
-    app.run(host ='127.0.0.1',port=5010,debug=True)
+    app.run(host ='0.0.0.0',port=5010,debug=True)
